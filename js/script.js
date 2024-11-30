ce5dce1839e70aa5c3589c2d9dd9890bd0ed7327
shuklavivek180797 <shuklavivek180797@gmail.com>
2024-11-30 12:47:32 +0530

    FE: Update Changes

--- js/script.js
+++ js/script.js
@@ -50,15 +50,16 @@ function sendEmail(event) {
 
 
 //    Send the email
-   emailjs.send("service_83jru2p", "template_ziby4bi", {
+   emailjs.send("service_ygmqpti", "template_sg1mtmx", {
        user_email: fromEmail,
-       to_email: "shuklavivek180797@gmail.com",
+       to_email: "nirvaanayurvedic.com@gmail.com",
        user_name: subject,
        message: message
    })
    .then(function(response) {
        console.log("Email sent successfully!", response);
-       alert("Email sent successfully!");
+       alert("Thank you for reaching us!");
+       window.location.href = "/";
    }, function(error) {
        console.error("Error sending email:", error);
        alert("Error sending email. Please try again later.");
@@ -68,135 +69,15 @@ function sendEmail(event) {
    return false;
  }
 
-
-function initiatePayment( paykey) {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var mobile = document.getElementById("mobile").value;
-    var address = document.getElementById("address").value;
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'))
-    const jsonFilePath = 'const.json';
-    fetch(jsonFilePath)
-                        .then(response => {
-                            if (!response.ok) {
-                                throw new Error('Network response was not ok');
-                            }
-                            return response.json();
-                        })
-                        .then(data => {
-                            let total_amount = 0;
-                            let packageName = [];
-                            // Compare each item in addItemArray with items in the JSON data and retrieve the amount
-                            cartItems.forEach((item, index) => {
-                                const matchingItem = data.package.find(packageItem => packageItem.name === item.name);
-                                total_amount += matchingItem.amount;
-                                packageName.push(matchingItem.name);
-                            })
-                            if(cartItems.length==1){
-                               total_amount = total_amount;
-                               console.log(name+" "+total_amount)
-                            }else if(cartItems.length==2){
-                               total_amount = total_amount - ((total_amount * 5) / 100)
-                               console.log(name+" "+total_amount)
-                            }else{
-                               total_amount = total_amount - ((total_amount * 10) / 100)
-                            }
-
-                            var options = {
-                                key: paykey,
-                                amount: total_amount * 100, // Amount in paise (100 paise = 1 INR)
-                                currency: "INR",
-                                name: "Nirvaan Ayurvedic Clinic",
-                                description: `Package: ${packageName.toString()} and
-                                              Address: ${address}`,
-                                image: 'images/logo.png', 
-                                handler: function(response) {
-                                    //Email send to User
-                                    emailjs.send("service_83jru2p","template_gqcdtor",{
-                                        user_email: email,
-                                        to_email: email,
-                                        user_name: name,
-                                        message: `Order placed 
-                                                  Yout payment id: ${response.razorpay_payment_id}
-                                                  Packages:  ${packageName.toString()}
-                                                  Total amount:  ${total_amount}
-                                                  Address:  ${address}`
-                                    })
-                                    .then(function(response) {
-                                        console.log("Email sent successfully!", response);
-                        
-                                    emailjs.send("service_83jru2p", "template_ziby4bi", {
-                                        user_email: email,
-                                        to_email: "shuklavivek180797@gmail.com",
-                                        user_name: name,
-                                        message: `Mobile no: ${mobile}
-                                                  Package:  ${packageName.toString()}
-                                                  Amount:  ${total_amount}
-                                                  Address:  ${address}`
-                                    })
-                                    .then(function(response) {
-                                        console.log("Email sent successfully!", response);
-                                        window.location.href = "index.html";
-                                    }, function(error) {
-                                        console.error("Error sending email:", error);
-                                    });
-                        
-                                    }, function(error) {
-                                        console.error("Error sending email:", error);
-                                    });
-                        
-                                    
-                                    localStorage.removeItem('cartItems');
-                                },
-                                prefill: {
-                                    name: name,
-                                    email: email,
-                                    contact: mobile
-                                }
-                            };
-                            var rzp1 = new Razorpay(options);
-                            rzp1.open();
-                        })
-                        .catch(error => {
-                            console.error('There was a problem fetching the JSON data:', error);
-                        });
-   
-    function getPackage(packages) {
-        let packageName = [];
-        for (let i = 0; i < packages.length; i++) {
-            packageName.push(packages[i].name);
-        }
-        return packageName.join(', ');
-    }    
-    const package = getPackage(cartItems);
-}
-
-// Add event listener to the button
-document.getElementById("rzp-button1").addEventListener("click", function(event) {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var mobile = document.getElementById("mobile").value;
-    var address = document.getElementById("address").value;
-    if(!name || !mobile || !address || !email){
-    }else{
-        const keyPath = 'key.json';
-let key;
-fetch(keyPath)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        key = data.PAYMENT_KEY;
-        initiatePayment(key);
-    })
-    .catch(error => {
-        console.error('Error fetching JSON:', error);
-    });
-    }
-
-    event.preventDefault(); // Prevent form submission
-});
+function toggleBotOptions() {
+    var options = document.getElementById("bot-options");
+    options.classList.toggle("hidden");
+  }
+  
+  function toggleBotOptions() {
+    var options = document.getElementById("bot-options");
+    var icon = document.getElementById("bot-icon");
+    options.classList.toggle("hidden");
+    icon.classList.toggle("rotated");
+  }
 
